1c9ace2aada4069800ac6dd77f3f20e7d0712fbd
Gary McDowell <garymcdowell@MacBook-Pro-de-Gary.local>
2020-10-07 15:55:35 +0200

    Updates to get real picture

---
+++ docs/src/.vuepress/components/OtherComponent2.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img :src="profile.image" /><!-- grab the profile picture -->
+  </div>
+</template>
+<script>
+  import "isomorphic-fetch" // keep IE happy
+  export default {
+    data() {
+      return {
+        profile: {}
+      };
+    }, 
+    mounted() {
+      this.getMastodonProfile();
+    },
+    methods: {
+      getMastodonProfile() {
+        // change this to use your account Id
+        fetch('https://simpatico.cloud/api/v1/getMask')
+          .then(stream => stream.json())
+          .then(data => this.profile = data)
+          .catch(error => console.error(error))			
+      }
+    }
+  }
+</script>
